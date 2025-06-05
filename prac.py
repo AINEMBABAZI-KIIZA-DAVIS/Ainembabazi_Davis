5257b73dadf611d1655c2ef5af66aa2c5582e9f8
Ainembabazi Kiiza Davis <ainembabazidavis1997@gmail.com>
2025-06-05 11:29:46 +0300

    Modified

--- prac.py
+++ prac.py
@@ -1,3 +1,4 @@
 Practice = 75
 print("My practice score is", Practice)
 print("This is a practice file for the Ainembabazi Davis project.")
+print("This file is used to practice Python coding skills.")

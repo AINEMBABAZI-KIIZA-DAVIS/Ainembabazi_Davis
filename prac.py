9cb4f79161cae5f78b1bed60b1a231f0ffa844a7
Ainembabazi Kiiza Davis <147053693+AINEMBABAZI-KIIZA-DAVIS@users.noreply.github.com>
2025-06-05 11:40:50 +0300

    Delete prac.py

--- prac.py
+++
@@ -1,4 +0,0 @@
-Practice = 75
-print("My practice score is", Practice)
-print("This is a practice file for the Ainembabazi Davis project.")
-print("This file is used to practice Python coding skills.")
